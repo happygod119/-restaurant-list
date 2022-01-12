d867231c62f6921e1b3190413f1a59216eb66a8a
happygod119 <happ0983493655@gmail.com>
2022-01-12 16:46:32 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 // require packages used in the project
 const express = require("express");
+const mongoose = require("mongoose"); //* 載入 mongoose
 const app = express();
 const port = 3000;
 
 // require express-handlebars here
 const exphbs = require("express-handlebars");
 const restaurantList = require("./restaurant.json");
+
+mongoose.connect("mongodb://localhost/Restaurant"); //* 設定連線到 mongoDB
+
+const db = mongoose.connection; //* 取得資料庫連線狀態
+
+//* 連線異常
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+//* 連線成功
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
 // setting template engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
