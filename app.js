98db1d63a1515f0407a131f510d532a21bd59b9d
happygod119 <happ0983493655@gmail.com>
2022-01-17 15:55:21 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -2,52 +2,28 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
 // 載入handlebars
 const exphbs = require("express-handlebars");
-
 //- 引用 body-parser
 const bodyParser = require("body-parser");
-
 //- 載入 method-override
 const methodOverride = require("method-override");
 
-//- 載入 mongoose
-const mongoose = require("mongoose");
 
-//- 設定連線到 mongoDB
-mongoose.connect("mongodb://localhost/restaurant-list");
 
-//- 取得資料庫連線狀態
-const db = mongoose.connection;
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
+require('./config/mongoose')//- 引用mongoose;
 
-//- 載入 Restaurant model
-const Restaurant = require("./models/restaurant");
 
-//- 引用路由器
-const routes = require("./routes");
+const routes = require("./routes");//- 引用路由器
 
-//設定使用handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));//設定使用handlebars
 app.set("view engine", "handlebars");
+app.use(express.static("public"));//使用public設定
+app.use(bodyParser.urlencoded({ extended: true }));//-使用body-parser
+app.use(methodOverride("_method"));//- 設定每一筆請求都會透過 methodOverride 進行前置處理
 
-//使用public設定
-app.use(express.static("public"));
-
-//-使用body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//- 設定每一筆請求都會透過 methodOverride 進行前置處理
-app.use(methodOverride("_method"));
-
-//- 將 request 導入路由器
-app.use(routes);
+app.use(routes);//- 將 request 導入路由器
 
 // 設定port
 app.listen(port, () => {
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,15 +1,6 @@
-const mongoose = require("mongoose");
 const Restaurant = require("../restaurant");
 const restaurantList = require("../../restaurant.json").results;
-
-mongoose.connect("mongodb://localhost/restaurant-list"); //* 設定連線到 mongoDB
-
-const db = mongoose.connection; //* 取得資料庫連線狀態
-
-//* 連線異常
-db.on("error", () => {
-  console.log("mongodb error!");
-});
+const db = require("../../config/mongoose");
 
 //* 連線成功
 db.once("open", () => {
