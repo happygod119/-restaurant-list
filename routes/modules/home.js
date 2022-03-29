2f79448f01ff83edc02bcff7426ad6f565366fa2
happygod119 <happ0983493655@gmail.com>
2022-03-29 20:52:07 +0800

    fix: Dropdown

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -20,7 +20,7 @@ router.get("/", (req, res) => {
 router.get("/search", (req, res) => {
   const keywords = req.query.keywords;
   const keyword = req.query.keywords.trim().toLowerCase();
-    const userId = req.user._id;
+  const userId = req.user._id;
   Restaurant.find({ userId })
     .lean()
     .then((restaurant) => {
@@ -41,35 +41,37 @@ router.get("/search", (req, res) => {
 router.get("/sort", (req, res) => {
   const sort = req.query.sort;
   let sortSelect = {};
-
+  const userId = req.user._id;
   if (Number(sort) === 1) {
     sortSelect = {
-      name_en: "asc"
+      name_en: "asc",
     };
   } else if (Number(sort) === 2) {
     sortSelect = {
-      name_en: "desc"
+      name_en: "desc",
     };
   } else if (Number(sort) === 3) {
     sortSelect = {
-      category: "asc"
+      category: "asc",
     };
   } else if (Number(sort) === 4) {
     sortSelect = {
-      location: "asc"
+      location: "asc",
     };
   } else {
     sortSelect = {
-      _id: "asc"
+      _id: "asc",
     };
   }
-  Restaurant.find()
+  Restaurant.find({ userId })
     .lean()
     .sort(sortSelect)
-    .then((restaurant) => res.render("index", {
-      restaurant,
-      sort
-    }))
+    .then((restaurant) =>
+      res.render("index", {
+        restaurant,
+        sort,
+      })
+    )
     .catch((error) => console.log(error));
 });
 module.exports = router;
